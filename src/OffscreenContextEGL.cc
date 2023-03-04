db04b0a0e55796ae2d99629487dfaaac9bd23a2e
Marius Kintel <marius@kintel.net>
2023-03-04 15:28:36 -0500

    Added eglGetErrorString()

--- src/OffscreenContextEGL.cc
+++ src/OffscreenContextEGL.cc
@@ -12,6 +12,31 @@
 
 namespace {
 
+#define CASE_STR( value ) case value: return #value; 
+const char* eglGetErrorString( EGLint error )
+{
+    switch( error )
+    {
+    CASE_STR( EGL_SUCCESS             )
+    CASE_STR( EGL_NOT_INITIALIZED     )
+    CASE_STR( EGL_BAD_ACCESS          )
+    CASE_STR( EGL_BAD_ALLOC           )
+    CASE_STR( EGL_BAD_ATTRIBUTE       )
+    CASE_STR( EGL_BAD_CONTEXT         )
+    CASE_STR( EGL_BAD_CONFIG          )
+    CASE_STR( EGL_BAD_CURRENT_SURFACE )
+    CASE_STR( EGL_BAD_DISPLAY         )
+    CASE_STR( EGL_BAD_SURFACE         )
+    CASE_STR( EGL_BAD_MATCH           )
+    CASE_STR( EGL_BAD_PARAMETER       )
+    CASE_STR( EGL_BAD_NATIVE_PIXMAP   )
+    CASE_STR( EGL_BAD_NATIVE_WINDOW   )
+    CASE_STR( EGL_CONTEXT_LOST        )
+    default: return "Unknown";
+    }
+}
+#undef CASE_STR
+
  void dumpEGLDisplay(EGLDisplay eglDisplay) {
    const EGLint configAttribs[] = {
     EGL_SURFACE_TYPE, EGL_PBUFFER_BIT,
@@ -338,7 +363,7 @@ std::shared_ptr<OffscreenContextEGL> OffscreenContextEGL::create(size_t width, s
 
   EGLint major, minor;
   if (!eglInitialize(ctx->eglDisplay, &major, &minor)) {
-    std::cerr << "Unable to initialize EGL" << std::endl;
+    std::cerr << "Unable to initialize EGL: " << eglGetErrorString(eglGetError()) << std::endl;
     return nullptr;
   }
 
