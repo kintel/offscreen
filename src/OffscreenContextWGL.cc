8befaec02eb8c5d85fac23d7a62312226e23ccf5
Marius Kintel <marius@kintel.net>
2023-03-08 13:12:01 -0500

    Initial implementation of WGL offscreen context

--- src/OffscreenContextWGL.cc
+++ src/OffscreenContextWGL.cc
@@ -7,9 +7,9 @@
 class OffscreenContextWGLImpl : public OffscreenContextWGL {
 
 public:
-  HWND window;
-  HDC devContext;
-  HGLRC renderContext;
+  HWND window = nullptr;
+  HDC devContext = nullptr;
+  HGLRC renderContext = nullptr;
 
   OffscreenContextWGLImpl(int width, int height) : OffscreenContextWGL(width, height) {}
   
@@ -18,9 +18,25 @@ public:
     return true;
   }
   bool destroy() override {
-    bool ok = wglDeleteContext(this->renderContext);
-    if (!ok) {
-      std::cerr << "wglDeleteContext() failed: " << GetLastError() << std::endl;
+    wglMakeCurrent(nullptr, nullptr);
+    bool ok = true;
+    if (this->renderContext) {
+      ok = wglDeleteContext(this->renderContext);
+      if (!ok) {
+        std::cerr << "wglDeleteContext() failed: " << GetLastError() << std::endl;
+      }
+    }
+    if (this->devContext) {
+      ok = ReleaseDC(this->window, this->devContext);
+      if (!ok) {
+        std::cerr << "ReleaseDC() failed: " << GetLastError() << std::endl;
+      }
+    }
+    if (this->window) {
+      ok = DestroyWindow(this->window);
+      if (!ok) {
+        std::cerr << "DestroyWindow() failed: " << GetLastError() << std::endl;
+      }
     }
     return ok;
   }
@@ -33,37 +49,35 @@ std::shared_ptr<OffscreenContextWGL> OffscreenContextWGL::create(size_t width, s
 
   WNDCLASSEX wndClass = {
     .cbSize = sizeof(WNDCLASSEX),
-    .style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC | CS_DBLCLKS,
+    .style = CS_OWNDC,
     .lpfnWndProc = &DefWindowProc,
-    .hCursor = LoadCursor(0, IDC_ARROW),
-    .hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH),
-    .lpszClassName = "WndClass"
+    .lpszClassName = "OffscreenClass"
   };
+  // FIXME: Check for ERROR_CLASS_ALREADY_EXISTS ?
   RegisterClassEx(&wndClass);
-  // style the window and remove the caption bar (WS_POPUP)
-  DWORD style = WS_CLIPSIBLINGS | WS_CLIPCHILDREN | WS_POPUP;
   // Create the window. Position and size it.
-  ctx->window = CreateWindowEx(0, "WndClass", "", style,
+  // Style the window and remove the caption bar (WS_POPUP)
+  ctx->window = CreateWindowEx(0, "OffscreenClass", "offscreen", WS_CLIPSIBLINGS | WS_CLIPCHILDREN | WS_POPUP,
     CW_USEDEFAULT, CW_USEDEFAULT, width, height, 0, 0, 0, 0);
   ctx->devContext = GetDC(ctx->window);
 
-  PIXELFORMATDESCRIPTOR pfd = {
+  PIXELFORMATDESCRIPTOR pixelFormatDesc = {
     .nSize = sizeof(PIXELFORMATDESCRIPTOR),
     .nVersion = 1,
+    // FIXME: Can we remove PFD_DOUBLEBUFFER for offscreen rendering?
     .dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER,
     .iPixelType = PFD_TYPE_RGBA,
     .cColorBits = 32,
-    .cDepthBits = 16,
-    .cStencilBits = 8,
-    .iLayerType = PFD_MAIN_PLANE
+    .cDepthBits = 24,
+    .cStencilBits = 8
   };
-  int iPixelFormat = ChoosePixelFormat(ctx->devContext, &pfd);
-  SetPixelFormat(ctx->devContext, iPixelFormat, &pfd);
+  int pixelFormat = ChoosePixelFormat(ctx->devContext, &pixelFormatDesc);
+  SetPixelFormat(ctx->devContext, pixelFormat, &pixelFormatDesc);
 
   ctx->renderContext = wglCreateContext(ctx->devContext);
   if (ctx->renderContext == nullptr) {
     std::cerr << "wglCreateContext() failed: " << GetLastError() << std::endl;
-    ReleaseDC(ctx->window, ctx->devContext);
+    ctx->destroy();
     return nullptr;
   }
   return ctx;
