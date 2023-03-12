75f1b31ea4e7810d52545631e7408495d9d7daa0
Marius Kintel <marius@kintel.net>
2023-03-12 19:52:06 -0400

    eglglad: enumerate devices

--- tools/eglglad/eglglad.cc
+++ tools/eglglad/eglglad.cc
@@ -4,6 +4,30 @@
 #define GLAD_EGL_IMPLEMENTATION
 #include "glad/egl.h"
 
+EGLDisplay tryDisplay(EGLDisplay display) {
+  if (display == EGL_NO_DISPLAY) return EGL_NO_DISPLAY;
+
+  EGLint major, minor;
+  if (!eglInitialize(display, &major, &minor)) {
+    std::cerr << "Unable to initialize display " << display << ": " << std::hex << eglGetError() << std::endl;
+    return EGL_NO_DISPLAY;
+  }
+
+  std::cout << "eglInitialize(" << display << "): EGL V" << major << "." << minor << std::endl;
+  
+  int actualEglVersion = gladLoaderLoadEGL(display);
+  if (!actualEglVersion) {
+    std::cerr << "gladLoaderLoadEGL(default display): Unable to load EGL" << std::endl;
+    return EGL_NO_DISPLAY;
+  }
+  std::cout << "Loaded EGL " << GLAD_VERSION_MAJOR(actualEglVersion) << "."
+    << GLAD_VERSION_MINOR(actualEglVersion) << " (default display)" << std::endl;
+
+  std::string extensions = eglQueryString(display, EGL_EXTENSIONS);
+  std::cout << "Display extensions: " << extensions << std::endl;
+  return display;
+}
+
 int main(int argc, char *argv[]) {
 
   int initialEglVersion = gladLoaderLoadEGL(NULL);
@@ -12,30 +36,33 @@ int main(int argc, char *argv[]) {
     return 1;
   }
   std::cout << "Loaded EGL " << GLAD_VERSION_MAJOR(initialEglVersion) << "."
-    << GLAD_VERSION_MINOR(initialEglVersion) << " on first load." << std::endl;
+    << GLAD_VERSION_MINOR(initialEglVersion) << " (no display)" << std::endl;
   std::string extensions = eglQueryString(EGL_NO_DISPLAY, EGL_EXTENSIONS);
   std::cout << "Client extensions: " << extensions << std::endl;
   
-  EGLDisplay display = eglGetDisplay(EGL_DEFAULT_DISPLAY);
-  EGLint major, minor;
-  if (eglInitialize(display, &major, &minor)) {
-    std::cout << "eglInitialize: EGL V" << major << "." << minor << std::endl;
-  } else {
-    std::cerr << "Unable to initialize default display " << display << ": " << std::hex << eglGetError() << std::endl;
-    return 1;
+  std::cout << "Trying default display..." << std::endl;
+  EGLDisplay display = tryDisplay(eglGetDisplay(EGL_DEFAULT_DISPLAY));
+  if (display == EGL_NO_DISPLAY) {
+    std::cout << "Unable to initialize default display" << std::endl;
+    if (eglQueryDevicesEXT && eglGetPlatformDisplayEXT) {
+      std::cout << "Trying eglQueryDevicesEXT()..." << std::endl;
+      EGLDeviceEXT device;
+      EGLint numDevices;
+      if (!eglQueryDevicesEXT(1, &device, &numDevices)) {
+        std::cerr << "eglQueryDevicesEXT() failed: " << std::hex << "0x" << eglGetError() << std::endl;
+        return 1;
+      }
+      if (numDevices == 0) {
+        std::cerr << "eglQueryDevicesEXT() returned no devices" << std::endl;
+        return 1;
+      }
+      display = tryDisplay(eglGetPlatformDisplayEXT(EGL_PLATFORM_DEVICE_EXT, device, nullptr));
+    }
   }
 
-  int actualEglVersion = gladLoaderLoadEGL(display);
-  if (!actualEglVersion) {
-    std::cerr << "gladLoaderLoadEGL(default display): Unable to load EGL" << std::endl;
-    return 1;
+  if (display == EGL_NO_DISPLAY) {
+    std::cout << "Unable to initialize display" << std::endl;
   }
-  std::cout << "Loaded EGL " << GLAD_VERSION_MAJOR(actualEglVersion) << "."
-    << GLAD_VERSION_MINOR(actualEglVersion) << " on second load." << std::endl;
-
-  extensions = eglQueryString(display, EGL_EXTENSIONS);
-  std::cout << "Display extensions: " << extensions << std::endl;
-
 
   return 0;
 }
