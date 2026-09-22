4f2b845b1de097bd99c476cf4547ea4495a9124c
Marius Kintel <marius@kintel.net>
2026-09-22 00:25:26 -0400

    CGL: use LOG instead of std::cerr, print error code, and label as CGL (new)

--- src/OffscreenContextCGL.cc
+++ src/OffscreenContextCGL.cc
@@ -1,34 +1,37 @@
 #include "OffscreenContextCGL.h"
 
 #include <cstddef>
-#include <iostream>
 #include <memory>
 #include <sstream>
 #include <string>
 
 #include "OffscreenContext.h"
 #include "system-gl.h"
+#include "utils/printutils.h"
 #define GL_SILENCE_DEPRECATION
 #include <OpenGL/OpenGL.h>
 
 class OffscreenContextCGL : public OffscreenContext {
 public:
-  OffscreenContextCGL(int width, int height) : OffscreenContext(width, height) {}
+  OffscreenContextCGL(int width, int height)
+      : OffscreenContext(width, height) {}
   ~OffscreenContextCGL() {
-    if (this->cglContext) {
-      CGLDestroyContext(this->cglContext);
+    if (cglContext) {
+      CGLDestroyContext(cglContext);
     }
   }
 
   std::string getInfo() const override {
     std::ostringstream out;
-    out << "GL context creator: CGL\n";
+    out << "GL context creator: CGL (new)\n";
     return out.str();
   }
 
   bool makeCurrent() const override {
-    if (CGLSetCurrentContext(this->cglContext) != kCGLNoError) {
-      std::cerr << "CGLSetCurrentContext() failed" << std::endl;
+    const auto err = CGLSetCurrentContext(cglContext);
+    if (err != kCGLNoError) {
+      LOG("CGLSetCurrentContext() failed: %1$s (%2$d)", CGLErrorString(err),
+          static_cast<int>(err));
       return false;
     }
     return true;
@@ -37,30 +40,31 @@ public:
   CGLContextObj cglContext = nullptr;
 };
 
-std::shared_ptr<OffscreenContext> CreateOffscreenContextCGL(size_t width, size_t height,
-                                                            size_t majorGLVersion, size_t minorGLVersion)
-{
+std::shared_ptr<OffscreenContext>
+CreateOffscreenContextCGL(size_t width, size_t height, size_t majorGLVersion,
+                          size_t minorGLVersion) {
   auto ctx = std::make_shared<OffscreenContextCGL>(width, height);
 
   CGLOpenGLProfile glVersion = kCGLOGLPVersion_Legacy;
-  if (majorGLVersion >= 4) glVersion = kCGLOGLPVersion_GL4_Core;
-  else if (majorGLVersion >= 3) glVersion = kCGLOGLPVersion_GL3_Core;
+  if (majorGLVersion >= 4)
+    glVersion = kCGLOGLPVersion_GL4_Core;
+  else if (majorGLVersion >= 3)
+    glVersion = kCGLOGLPVersion_GL3_Core;
 
   CGLPixelFormatAttribute attributes[13] = {
-    kCGLPFAOpenGLProfile,
-    (CGLPixelFormatAttribute)glVersion,
-    kCGLPFAColorSize, (CGLPixelFormatAttribute)24,
-    kCGLPFAAlphaSize, (CGLPixelFormatAttribute)8,
-    kCGLPFADoubleBuffer,
-    kCGLPFASampleBuffers, (CGLPixelFormatAttribute)1,
-    kCGLPFASamples,  (CGLPixelFormatAttribute)4,
-    (CGLPixelFormatAttribute) 0
-  };
+      kCGLPFAOpenGLProfile,       (CGLPixelFormatAttribute)glVersion,
+      kCGLPFAColorSize,           (CGLPixelFormatAttribute)24,
+      kCGLPFAAlphaSize,           (CGLPixelFormatAttribute)8,
+      kCGLPFADoubleBuffer,        kCGLPFASampleBuffers,
+      (CGLPixelFormatAttribute)1, kCGLPFASamples,
+      (CGLPixelFormatAttribute)4, (CGLPixelFormatAttribute)0};
   CGLPixelFormatObj pixelFormat = NULL;
   GLint numPixelFormats = 0;
-  const auto status = CGLChoosePixelFormat(attributes, &pixelFormat, &numPixelFormats);
+  const auto status =
+      CGLChoosePixelFormat(attributes, &pixelFormat, &numPixelFormats);
   if (status != kCGLNoError) {
-    std::cerr << "CGLChoosePixelFormat() failed: " << CGLErrorString(status) << std::endl;
+    LOG("CGLChoosePixelFormat() failed: %1$s (%2$d)", CGLErrorString(status),
+        static_cast<int>(status));
     return nullptr;
   }
   CGLCreateContext(pixelFormat, NULL, &ctx->cglContext);
@@ -68,4 +72,3 @@ std::shared_ptr<OffscreenContext> CreateOffscreenContextCGL(size_t width, size_t
 
   return ctx;
 }
-
